db8bbec449596e30d0e08689b53c3ac7470f2648
Omar Shribati <omarshribati10@gmail.com>
2026-05-07 22:48:15 +0300

    makenotificationnnn

--- PTP/urls/auth_urls.py
+++ PTP/urls/auth_urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from PTP.views.auth_views import LoginView, RegistrationView
 from PTP.views.logout_views import LogoutView
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name='auth-register'),
-    path('login/', LoginView.as_view(), name='auth-login'),
-    path('logout/', LogoutView.as_view(), name='auth-logout'),
+    path('register', RegistrationView.as_view(), name='auth-register'),
+    path('login', LoginView.as_view(), name='auth-login'),
+    path('logout', LogoutView.as_view(), name='auth-logout'),
 ]
